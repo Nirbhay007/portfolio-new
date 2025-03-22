7043c8778b49345c89d34293da38b9eeb8b2f727
nirbhay.singh <nirbhay.singh@atherenergy.com>
2025-03-22 16:41:02 +0530

    feat: adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -49,6 +51,8 @@ export default function RootLayout({
           <Header />
           <main className="relative">{children}</main>
           <Toaster />
+          <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
